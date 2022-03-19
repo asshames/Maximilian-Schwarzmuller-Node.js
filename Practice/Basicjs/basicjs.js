36271cf3767236e5997eb06bae424abaca621c57
asshames <assamialomarshames@gmail.com>
2022-03-19 18:29:18 +0600

    using array and object

--- Practice/Basicjs/basicjs.js
+++ Practice/Basicjs/basicjs.js
@@ -5,17 +5,23 @@ const hasHobbies = true;
 
 age = 30;
 
-function summarizeUser(userName, userAge, userHasHobby, UserschoolName) {
+const summarizeUser = (userName, userAge, userHasHobby) => {
   return (
     'Name is ' +
     userName +
     ', age is ' +
     userAge +
     ' and the user has hobbies: ' +
-    userHasHobby +
-    ' and the user\'s school name: ' +
-    UserschoolName
+    userHasHobby
   );
-}
+};
 
-console.log(summarizeUser(name, age, hasHobbies, schoolName));
+// const add = (a, b) => a + b;
+// const addOne = a => a + 1;
+const addRandom = () => 1 + 2;
+
+// console.log(add(1, 2));
+// console.log(addOne(1));
+console.log(addRandom());
+
+console.log(summarizeUser(name, age, hasHobbies));
